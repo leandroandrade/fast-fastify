6e65b80c630851994a02f6d72db587200cf8ab09
leandroandrade <leandroandrade2588@gmail.com>
2020-03-31 07:26:27 -0300

    Add tests with mongodb-memory-server

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -1,33 +1,33 @@
 'use strict';
 
 const supertest = require('supertest');
-// const { MongoMemoryServer } = require('mongodb-memory-server');
+const { MongoMemoryServer } = require('mongodb-memory-server');
 
-// const mongoServer = new MongoMemoryServer();
+const mongoServer = new MongoMemoryServer();
 const fastify = require('../../src/app');
 
 describe('Exemplo', () => {
 
     beforeAll(async () => {
-        // process.env.MONGODB_URI = await mongoServer.getUri();
+        process.env.MONGODB_URI = await mongoServer.getUri();
 
         await fastify.ready();
     });
 
     afterAll(async () => {
         await fastify.close();
-        // await mongoServer.stop();
+        await mongoServer.stop();
     });
 
 
     it('deve executar exemplo de teste', async done => {
         const { status, body } = await supertest(fastify.server)
-            .get('/api')
+            .get('/api/hello')
             .expect(200)
             .expect('Content-Type', 'application/json; charset=utf-8');
 
         expect(status).toBe(200);
-        expect(body).toStrictEqual({ hello: 'WORLD', lower: 'lower' });
+        expect(body).toStrictEqual({ hello: 'WORLD' });
 
         done();
     });
